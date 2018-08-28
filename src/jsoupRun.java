059edfcec399fe4bd594a8c4d9dc9a0087acc97b
vish25v <vaishakhi.verma25@gmail.com>
2018-08-28 15:09:45 -0400

    implemented exception handling and formated the output to give stream of only values

--- src/jsoupRun.java
+++ src/jsoupRun.java
@@ -1,40 +1,45 @@
 package demo;
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
-  import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.DataNode;
+public class demorun2 {
 
-  public class jsoupRun {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		Document doc =Jsoup.connect("https://www.instagram.com/p/BY07aH0F2r1/?taken-by=earthpix").timeout(6000).get();
 
-      public static void main(String[] args) {
+		Elements script = doc.select("script");
+		//Pattern p = Pattern.compile("xmlhttp.open\\(\"GET\", \"(.*)\", false\\)")		                                                    //  ^^ is the capturing group 
+		
+		
+		String pattern1 = "\"text\":";
+	    String pattern2 = ",";
+			  
+		Pattern p = Pattern.compile(Pattern.quote(pattern1) + "(.*?)" + Pattern.quote(pattern2));
+		
+		String url = "";
+		 
+		for (Element element : script) {
+		    Matcher m = p.matcher(element.data());
+		   /* if (m.find()){
+		        url = m.group(1);
+		        //System.out.println(url);
+//				   System.out.println(m.group(1));
 
-          String htmlString = "<html><head><title>My title</title></head>"
-                  + "<body>Body content</body></html>";
+		    }*/
+		    while (m.find()) {
+				   System.out.println(m.group(1));
+				 } 
+		} 
+		System.out.println(url);
 
-          Document doc = Jsoup.parse(htmlString);
-          String title = doc.title();
-          String body = doc.body().text();
+	}
 
-          System.out.printf("Title: %s%n", title);
-          System.out.printf("Body: %s", body);
-          Document doc =Jsoup.connect("https://www.instagram.com/p/BY6gh6Pl6Fk/?hl=en&taken-by=earthpix").timeout(6000).get();
-
-
- 		 Element script = doc.select("script").first(); // Get the script part
- 				   
- 				   
- 				  Pattern p = Pattern.compile("(?is)key=\"(.+?)\""); // Regex for the value of the key
- 				  Matcher m = p.matcher(script.html()); // you have to use html here and NOT text! Text will drop the 'key' part
- 				   
- 				   
- 				  while( m.find() )
- 				  { 
- 				      System.out.println(m.group()); // the whole key ('key = value')
- 				      System.out.println(m.group(1)); // value only
- 				  }
- 		  
-      
-  }
 }
